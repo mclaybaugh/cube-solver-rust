21d27fac270f02a7c071c462b21be45dbcbe4a4f
Michael Claybaugh <michael.claybaugh@gmail.com>
2022-03-12 21:36:23 -0500

    finish rotate and scramble

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 // Using color initials for code to make it more readable, but storing
 // colors as ints in tuples
 const W: u8 = 0;
@@ -84,40 +86,25 @@ impl PocketCube {
     // face 2,0, 110 > 100 > 000 > 010 > 110 (11,10,00,01) (from bottom)
     // face 2,1, 111 > 011 > 001 > 101 > 111 (11,01,00,10) (from top)
     // 1/2 flip
-    fn rotate(cube: &PocketCube, face: &Face) -> PocketCube {
+    fn rotate(cube: PocketCube, face: Face) -> PocketCube {
         let mut new_cube = cube.clone();
         for piece in new_cube.pieces.iter_mut() {
             if piece.position[face.axis] == face.value {
-                // Update position
-                if (face.axis == 0 && face.value == 0)
-                    || (face.axis == 1 && face.value == 1)
-                    || (face.axis == 2 && face.value == 0)
-                {
-                    // 11 -> 10
-                } else {
-                    // 11 -> 01
-                }
-
-                // Update orientation
-                match face.axis {
-                    0 => {
-                        piece.orientation = swap_3(&mut piece.orientation, 0, 1);
-                    }
-                    1 => {
-                        piece.orientation = swap_3(&mut piece.orientation, 0, 2);
-                    }
-                    _ => {
-                        piece.orientation = swap_3(&mut piece.orientation, 1, 2);
-                    }
-                }
+                piece.position = rotate_position(piece.position, &face);
+                piece.orientation = rotate_orientation(piece.orientation, face.axis);
             }
         }
         return new_cube;
     }
 
     // Will perform n random rotations on cube and return.
-    fn scramble(cube: PocketCube, n: u8) -> PocketCube {
-        return cube.clone();
+    fn scramble(mut cube: PocketCube, n: u8) -> PocketCube {
+        for _ in 0..n {
+            let x: u8 = rand::thread_rng().gen_range(0..6);
+            let face = get_face(x);
+            cube = PocketCube::rotate(cube, face);
+        }
+        return cube;
     }
 
     fn is_solved(cube: PocketCube) -> bool {
@@ -130,11 +117,73 @@ impl PocketCube {
     }
 }
 
-fn swap_3(orientation: &mut [u8; 3], i: usize, j: usize) -> [u8; 3] {
+fn rotate_position(pos: [u8; 3], face: &Face) -> [u8; 3] {
+    let indexes: (usize, usize) = get_other_indexes(face.axis);
+    let v1 = pos[indexes.0];
+    let v2 = pos[indexes.1];
+    let new_vals: (u8, u8);
+    if (face.axis == 0 && face.value == 0)
+        || (face.axis == 1 && face.value == 1)
+        || (face.axis == 2 && face.value == 0)
+    {
+        // 11 -> 10
+        new_vals = match (v1, v2) {
+            (1, 1) => (1, 0),
+            (1, 0) => (0, 0),
+            (0, 0) => (0, 1),
+            (0, 1) => (1, 1),
+            _ => (1, 1),
+        };
+    } else {
+        // 11 -> 01
+        new_vals = match (v1, v2) {
+            (1, 1) => (0, 1),
+            (0, 1) => (0, 0),
+            (0, 0) => (1, 0),
+            (1, 0) => (1, 1),
+            _ => (1, 1),
+        };
+    }
+    let mut new_pos = [0, 0, 0];
+    new_pos[indexes.0] = new_vals.0;
+    new_pos[indexes.1] = new_vals.1;
+    new_pos[face.axis] = pos[face.axis];
+    return pos;
+}
+
+fn rotate_orientation(orientation: [u8; 3], axis: usize) -> [u8; 3] {
+    match axis {
+        0 => swap_3(orientation, 0, 1),
+        1 => swap_3(orientation, 0, 2),
+        _ => swap_3(orientation, 1, 2),
+    }
+}
+
+fn get_other_indexes(i: usize) -> (usize, usize) {
+    match i {
+        0 => (1, 2),
+        1 => (0, 2),
+        _ => (1, 2),
+    }
+}
+
+fn get_face(x: u8) -> Face {
+    match x {
+        0 => Face { axis: 0, value: 0 },
+        1 => Face { axis: 0, value: 1 },
+        2 => Face { axis: 1, value: 0 },
+        3 => Face { axis: 1, value: 1 },
+        4 => Face { axis: 2, value: 0 },
+        5 => Face { axis: 2, value: 1 },
+        _ => Face { axis: 0, value: 0 },
+    }
+}
+
+fn swap_3(mut orientation: [u8; 3], i: usize, j: usize) -> [u8; 3] {
     let temp = orientation[i];
     orientation[i] = orientation[j];
     orientation[j] = temp;
-    return *orientation;
+    return orientation;
 }
 
 fn main() {
