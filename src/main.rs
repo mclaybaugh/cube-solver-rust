9e921f036d9d3f7abb51a7df0ba10daefe9d62b9
Michael Claybaugh <michael.claybaugh@gmail.com>
2022-04-03 18:07:27 -0400

    test with 2 moves to scramble and solve

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use cube::Pocket;
 
 fn main() {
     let solved_cube = Pocket::new();
-    let (scrambled, moves) = scramble(&solved_cube, 1);
+    let (scrambled, moves) = scramble(&solved_cube, 2);
     println!("scramble moves: {:?}", moves);
 
-    let result = maybe_solve_in(&scrambled, 1);
+    let result = maybe_solve_in(&scrambled, 2);
     println!("maybe solve: {:?}", result);
 }
