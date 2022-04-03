2b95b8c7d39eb9d34770b2dc64c90081e25613aa
Michael Claybaugh <michael.claybaugh@gmail.com>
2022-04-03 18:05:16 -0400

    working rotation as enum

--- src/cube/mod.rs
+++ src/cube/mod.rs
@@ -2,24 +2,24 @@ mod color;
 mod rotation;
 
 use color::Color;
-use rand::Rng;
 use rotation::Face;
+use rotation::Rdir;
 use rotation::Rotation;
 use rotation::RotationDetails;
 use std::fmt;
 
 pub trait Cube {
-    fn rotate(&self, rotation_index: u8) -> Self;
+    fn rotate(&self, r: Rotation) -> Self;
     fn is_solved(&self) -> bool;
 }
 
 // Perform n random rotations on cube and return.
 // TODO make this work on any type with trait of "puzzle_cube"
-pub fn scramble<T: Cube + Clone>(cube: &T, n: u8) -> (T, Vec<u8>) {
-    let mut rotations: Vec<u8> = Vec::new();
+pub fn scramble<T: Cube + Clone>(cube: &T, n: u8) -> (T, Vec<Rotation>) {
+    let mut rotations: Vec<Rotation> = Vec::new();
     let mut new_cube = cube.clone();
     for _ in 0..n {
-        let rotation_index = random_rotation_index();
+        let rotation_index = Rotation::random();
         rotations.push(rotation_index);
         new_cube = new_cube.rotate(rotation_index);
     }
@@ -29,11 +29,11 @@ pub fn scramble<T: Cube + Clone>(cube: &T, n: u8) -> (T, Vec<u8>) {
 // Brute force every move and return true if one solves it.
 // iterative deepening depth-first traversal
 // needs to return breadcrumb tail on success, or false/None
-pub fn maybe_solve_in<T: Cube + Clone>(cube: &T, n: u8) -> (Option<Vec<u8>>, u64) {
+pub fn maybe_solve_in<T: Cube + Clone>(cube: &T, n: u8) -> (Option<Vec<Rotation>>, u64) {
     let mut depth = 0;
     let mut nodes_checked = 0;
     while depth <= n {
-        let moves: Vec<u8> = Vec::new();
+        let moves: Vec<Rotation> = Vec::new();
         let (returned_moves, is_solved, nodes_checked_r) =
             depth_limited_search(cube.clone(), &moves, depth, nodes_checked);
         nodes_checked = nodes_checked_r;
@@ -47,10 +47,10 @@ pub fn maybe_solve_in<T: Cube + Clone>(cube: &T, n: u8) -> (Option<Vec<u8>>, u64
 
 fn depth_limited_search<T: Cube + Clone>(
     cube: T,
-    moves: &Vec<u8>,
+    moves: &Vec<Rotation>,
     n: u8,
     mut nodes_checked: u64,
-) -> (Vec<u8>, bool, u64) {
+) -> (Vec<Rotation>, bool, u64) {
     // end of line, check if solved
     if n == 0 {
         if cube.is_solved() {
@@ -61,22 +61,22 @@ fn depth_limited_search<T: Cube + Clone>(
     }
 
     // do check on children (rotations)
-    for i in 0..12 {
+    for r in Rotation::array() {
         let move_len = moves.len();
         // don't reverse prior move
-        if move_len > 0 && is_reverse_rotation(moves[move_len - 1], i) {
+        if move_len > 0 && r.is_reverse(moves[move_len - 1]) {
             continue;
         }
         // don't do same move 4 times in a row
         if move_len > 3
-            && (moves[move_len - 1] == i && moves[move_len - 2] == i && moves[move_len - 3] == i)
+            && (moves[move_len - 1] == r && moves[move_len - 2] == r && moves[move_len - 3] == r)
         {
             continue;
         }
 
         let mut new_moves = moves.clone();
-        new_moves.push(i);
-        let ncube = cube.rotate(i);
+        new_moves.push(r);
+        let ncube = cube.rotate(r);
         let (new_moves, solved, nodes_checked_r) =
             depth_limited_search(ncube, &new_moves, n - 1, nodes_checked);
         nodes_checked = nodes_checked_r;
@@ -85,22 +85,6 @@ fn depth_limited_search<T: Cube + Clone>(
         }
     }
     return (moves.clone(), false, nodes_checked);
-}
-
-fn random_rotation_index() -> u8 {
-    rand::thread_rng().gen_range(0..12)
-}
-
-fn is_reverse_rotation(i: u8, j: u8) -> bool {
-    match (i, j) {
-        (0, 1) | (1, 0) => true,
-        (2, 3) | (3, 2) => true,
-        (4, 5) | (5, 4) => true,
-        (6, 7) | (7, 6) => true,
-        (8, 9) | (9, 8) => true,
-        (10, 11) | (11, 10) => true,
-        _ => false,
-    }
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -381,8 +365,8 @@ impl Cube for Pocket {
     // face 2,0, 110 > 100 > 000 > 010 > 110 (11,10,00,01) (from bottom)
     // face 2,1, 111 > 011 > 001 > 101 > 111 (11,01,00,10) (from top)
     // 1/2 flip
-    fn rotate(&self, rotation_index: u8) -> Pocket {
-        let rotation = get_rotation(rotation_index);
+    fn rotate(&self, r: Rotation) -> Pocket {
+        let rotation = r.details();
         let mut new_cube = self.clone();
         for piece in new_cube.corners.iter_mut() {
             if piece.position[rotation.face.axis] == rotation.face.value {
@@ -440,7 +424,7 @@ mod test {
 
     #[test]
     fn rotates_left_clockwise_correctly() {
-        let cube = Pocket::new().rotate(0);
+        let cube = Pocket::new().rotate(Rotation::L);
         let ref_cube = Pocket::from_array([
             [0, 0, 1, 5, 0, 3],
             [0, 1, 1, 5, 1, 3],
--- src/cube/rotation.rs
+++ src/cube/rotation.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum Rdir {
     Clockwise,
@@ -21,22 +23,111 @@ pub enum Rotation {
     Tc,
 }
 
-impl Iterator for Rotation {
-    type Item = Rotation;
-    fn next(&mut self) -> Option<Rotation> {
+impl Rotation {
+    pub fn details(&self) -> RotationDetails {
         match self {
-            L => Some(Rotation::Lc),
-            Lc => Some(Rotation::R),
-            R => Some(Rotation::Rc),
-            Rc => Some(Rotation::Ba),
-            Ba => Some(Rotation::Bac),
-            Bac => Some(Rotation::F),
-            F => Some(Rotation::Fc),
-            Fc => Some(Rotation::Bo),
-            Bo => Some(Rotation::Boc),
-            Boc => Some(Rotation::T),
-            T => Some(Rotation::Tc),
-            Tc => None,
+            L => RotationDetails {
+                face: Face { axis: 0, value: 0 },
+                direction: Rdir::Clockwise,
+            },
+            Lc => RotationDetails {
+                face: Face { axis: 0, value: 0 },
+                direction: Rdir::CounterClockwise,
+            },
+            R => RotationDetails {
+                face: Face { axis: 0, value: 1 },
+                direction: Rdir::Clockwise,
+            },
+            Rc => RotationDetails {
+                face: Face { axis: 0, value: 1 },
+                direction: Rdir::CounterClockwise,
+            },
+            Ba => RotationDetails {
+                face: Face { axis: 1, value: 0 },
+                direction: Rdir::Clockwise,
+            },
+            Bac => RotationDetails {
+                face: Face { axis: 1, value: 0 },
+                direction: Rdir::CounterClockwise,
+            },
+            F => RotationDetails {
+                face: Face { axis: 1, value: 1 },
+                direction: Rdir::Clockwise,
+            },
+            Fc => RotationDetails {
+                face: Face { axis: 1, value: 1 },
+                direction: Rdir::CounterClockwise,
+            },
+            Bo => RotationDetails {
+                face: Face { axis: 2, value: 0 },
+                direction: Rdir::Clockwise,
+            },
+            Boc => RotationDetails {
+                face: Face { axis: 2, value: 0 },
+                direction: Rdir::CounterClockwise,
+            },
+            T => RotationDetails {
+                face: Face { axis: 2, value: 1 },
+                direction: Rdir::Clockwise,
+            },
+            Tc => RotationDetails {
+                face: Face { axis: 2, value: 1 },
+                direction: Rdir::CounterClockwise,
+            },
+        }
+    }
+
+    pub fn random() -> Rotation {
+        let i = rand::thread_rng().gen_range(0..12);
+        Rotation::by_index(i)
+    }
+
+    pub fn by_index(i: u8) -> Rotation {
+        match i {
+            0 => Rotation::L,
+            1 => Rotation::Lc,
+            2 => Rotation::R,
+            3 => Rotation::Rc,
+            4 => Rotation::Ba,
+            5 => Rotation::Bac,
+            6 => Rotation::F,
+            7 => Rotation::Fc,
+            8 => Rotation::Bo,
+            9 => Rotation::Boc,
+            10 => Rotation::T,
+            11 => Rotation::Tc,
+            _ => {
+                panic!("Invalid rotation index: {}", i);
+            }
+        }
+    }
+
+    pub fn array() -> [Rotation; 12] {
+        [
+            Rotation::L,
+            Rotation::Lc,
+            Rotation::R,
+            Rotation::Rc,
+            Rotation::Ba,
+            Rotation::Bac,
+            Rotation::F,
+            Rotation::Fc,
+            Rotation::Bo,
+            Rotation::Boc,
+            Rotation::T,
+            Rotation::Tc,
+        ]
+    }
+
+    pub fn is_reverse(&self, other: Rotation) -> bool {
+        match (self, other) {
+            (Rotation::L, Rotation::Lc) | (Rotation::Lc, Rotation::L) => true,
+            (Rotation::R, Rotation::Rc) | (Rotation::Rc, Rotation::R) => true,
+            (Rotation::Ba, Rotation::Bac) | (Rotation::Bac, Rotation::Ba) => true,
+            (Rotation::F, Rotation::Fc) | (Rotation::Fc, Rotation::F) => true,
+            (Rotation::Bo, Rotation::Boc) | (Rotation::Boc, Rotation::Bo) => true,
+            (Rotation::T, Rotation::Tc) | (Rotation::Tc, Rotation::T) => true,
+            _ => false,
         }
     }
 }
@@ -44,59 +135,6 @@ impl Iterator for Rotation {
 pub struct RotationDetails {
     pub face: Face,
     pub direction: Rdir,
-}
-
-fn get_rotation(r: Rotation) -> RotationDetails {
-    match r {
-        L => RotationDetails {
-            face: Face { axis: 0, value: 0 },
-            direction: Rdir::Clockwise,
-        },
-        Lc => RotationDetails {
-            face: Face { axis: 0, value: 0 },
-            direction: Rdir::CounterClockwise,
-        },
-        R => RotationDetails {
-            face: Face { axis: 0, value: 1 },
-            direction: Rdir::Clockwise,
-        },
-        Rc => RotationDetails {
-            face: Face { axis: 0, value: 1 },
-            direction: Rdir::CounterClockwise,
-        },
-        Ba => RotationDetails {
-            face: Face { axis: 1, value: 0 },
-            direction: Rdir::Clockwise,
-        },
-        Bac => RotationDetails {
-            face: Face { axis: 1, value: 0 },
-            direction: Rdir::CounterClockwise,
-        },
-        F => RotationDetails {
-            face: Face { axis: 1, value: 1 },
-            direction: Rdir::Clockwise,
-        },
-        Fc => RotationDetails {
-            face: Face { axis: 1, value: 1 },
-            direction: Rdir::CounterClockwise,
-        },
-        Bo => RotationDetails {
-            face: Face { axis: 2, value: 0 },
-            direction: Rdir::Clockwise,
-        },
-        Boc => RotationDetails {
-            face: Face { axis: 2, value: 0 },
-            direction: Rdir::CounterClockwise,
-        },
-        T => RotationDetails {
-            face: Face { axis: 2, value: 1 },
-            direction: Rdir::Clockwise,
-        },
-        Tc => RotationDetails {
-            face: Face { axis: 2, value: 1 },
-            direction: Rdir::CounterClockwise,
-        },
-    }
 }
 
 // To select the bottom face, axis would be Z (position tuple index of 2) and
